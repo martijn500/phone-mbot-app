bce06f4688a424235bf54a6be57873c299ff7e29
jorrit <jorrit.van.der.ven@jdriven.com>
2019-07-05 17:17:17 +0200

    fix btn size

--- dist/phone-mbot-app.e31bb0bc.js
+++ dist/phone-mbot-app.e31bb0bc.js
@@ -503,7 +503,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56913" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56948" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
